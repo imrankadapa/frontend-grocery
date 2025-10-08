ab1c23b864046b9d6ea361ee2dfdd6e840054f1c
imrankadapa <kadapaimran99@outlook.com>
2025-10-08 22:10:27 +0530

    Update authService.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:9090/springapp1/auth";
+const API_URL = "http://3.145.212.235:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });

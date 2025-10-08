c7542ccfeeb4f9f1d90dbff13f803a2fa1306f07
imrankadapa <kadapaimran99@outlook.com>
2025-10-08 22:53:35 +0530

    msg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/frontapp1/',   // or './' if you prefer relative
+     // or './' if you prefer relative
   plugins: [react()]
 })
